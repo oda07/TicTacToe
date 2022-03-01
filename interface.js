a36554e3fa8c1267a8de2fc777c706521ec97692
Odair Lampe <odairlampe123@gmail.com>
2022-03-01 00:30:09 -0300

    game ready to play

---
+++ interface.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+        square.addEventListener("click", handleClick);
+    })
+})
+
+function handleClick(event){
+    
+    let square = event.target;
+    let position = square.id;
+
+    handleMove(position);
+    updateSquares();
+
+}
+
+function updateSquares(){
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+        let position = square.id;
+        let symbol = board[position];
+
+        if(symbol != ''){
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
+
+}
+
+function reiniciar(){
+    board = ['', '', '', '', '', '', '', '', '',];
+    playerTime = 0;
+    gameOver = false;
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+            square.innerHTML = '';
+        })
+}
+
